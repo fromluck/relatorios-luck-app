0d76149a55f7d166faf4475a8d6e368738b87a76
fromluck <contato.fromluck@gmail.com>
2026-05-29 09:25:16 -0300

    Conecta Supabase config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LUCK_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://cvvgxhfdrmkuknjipmoz.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dmd4aGZkcm1rdWtuamlwbW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNDY1OTksImV4cCI6MjA5NTYyMjU5OX0.XxnmEGiSD_L3pHpqVKQ-FlDM6J1KcO_2YlFLyYvs_9o"
 };
