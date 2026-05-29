8b1dea6dcacac12180143970c2f1592839505766
fromluck <contato.fromluck@gmail.com>
2026-05-29 13:04:21 -0300

    Prepara login com Google config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.LUCK_SUPABASE = {
   url: "https://cvvgxhfdrmkuknjipmoz.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dmd4aGZkcm1rdWtuamlwbW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNDY1OTksImV4cCI6MjA5NTYyMjU5OX0.XxnmEGiSD_L3pHpqVKQ-FlDM6J1KcO_2YlFLyYvs_9o"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dmd4aGZkcm1rdWtuamlwbW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNDY1OTksImV4cCI6MjA5NTYyMjU5OX0.XxnmEGiSD_L3pHpqVKQ-FlDM6J1KcO_2YlFLyYvs_9o",
+  googleAuthEnabled: false
 };
